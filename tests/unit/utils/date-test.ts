d3266f2848a394bc5a158ff0b826eb4977d7f119
Kristof Dreier <mail@kristofdreier.de>
2026-03-29 23:15:51 +0200

    Fix test

--- tests/unit/utils/date-test.ts
+++ tests/unit/utils/date-test.ts
@@ -2,11 +2,11 @@ import { module, test } from 'qunit';
 import { formatDateTime } from 'potber-client/utils/date';
 
 module('Unit | Utils | date', function () {
-  test('formats dates in short German date-time format', function (assert) {
-    const formatted = formatDateTime(new Date(2025, 2, 27, 14, 5));
+  test('formats dates in medium German date-time format', function (assert) {
+    const formatted = formatDateTime(new Date(2025, 2, 27, 14, 5, 23));
 
-    assert.true(formatted.includes('27.03.25'));
-    assert.true(formatted.includes('14:05'));
+    assert.true(formatted.includes('27.03.2025'));
+    assert.true(formatted.includes('14:05:23'));
   });
 
   test('returns an empty string for missing values', function (assert) {
